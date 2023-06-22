5ebecdb2f765cb173ebdfe0652786117627c3ba1
Daniil Borovov <borovovdaniil@gmail.com>
2023-06-23 04:24:52 +0600

    add admin id to middleware

--- backend/middlewares/check-card-owner.js
+++ backend/middlewares/check-card-owner.js
@@ -5,12 +5,16 @@ const {
   NotFoundError,
 } = require('../utils/errors');
 
+// temp admin id
+const adminID = '648ab71fe3e10613f490ce25';
+
 module.exports = (req, res, next) => {
   Card.findById(req.params.cardId)
     .then((card) => {
       if (!card) {
         next(new NotFoundError(`Нет карточки с id ${req.params.cardId}`));
-      } else if (card.owner.toString() !== req.user._id) {
+        // temp admin id
+      } else if (card.owner.toString() !== req.user._id && req.user._id !== adminID) {
         next(new ForbiddenError('Нет прав на удаление'));
       } else {
         next();
